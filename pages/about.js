288de6abb7522b9eb5fef731c6d4c29b2598921f
reyyymartt <reymartcenteno03@gmail.com>
2024-09-11 09:48:44 +0800

    update

---
+++ pages/about.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import HomePage from './home'
+import navigate from '/app'
+
+
+class APage extends React.Component{
+ 
+ render (){
+  const cardstyle = {
+   margin:'5px'
+  }
+  return (
+   <div>
+   <div style={cardstyle} className="card">
+   <div className="card-body">
+   <h3 className="card-title">{this.props.tit}</h3>
+   <p className="card-text">{this.props.txt}</p>
+   </div>
+   </div>
+   <button style={{margin:'5px'}} onClick={
+   function (){
+   navigate('home')
+   }
+   } className="btn btn-danger" >Change to home</button>
+   </div>
+   )
+ }
+}
+
+export default function AboutPage() {
+ return (
+  <APage tit='Test title' txt='Sup?'/>
+  )
+}
---
+++ pages/developers.js
@@ -0,0 +1,55 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const devs = [
+ {id:1,name:'Rey',des:'Hi?',img:'/assets/me.jpg'},
+ {id:2,name:'Jasper',img:'...'},
+ {id:3,name:'Jed',img:'...'},
+ {id:4,name:'Regie',img:'...'}
+]
+
+class MainPage extends React.Component{
+ 
+ 
+ 
+ 
+ render (){
+  const cardbod={
+   margin:'5px'
+  }
+  
+  const imgStyle={
+   width:'100px',
+   height:'100px',
+   objectFit:'cover'
+  }
+  
+  const devslist = devs.map(dev=>
+  <div style={cardbod} key={dev.id} className="text-center col-sm-5 bg-light card-body">
+  <img style={imgStyle} src={dev.img} alt={dev.name} className="rounded"></img>
+  <i><h5>{dev.name}</h5></i>
+  <p>{dev.des?dev.des:'description'}</p>
+  </div>
+  )
+  
+  return (
+   <div>
+   <div className="container">
+   
+   <div className="row">
+   {devslist}
+   </div>
+   
+   </div>
+   
+   
+   </div>
+  )
+ }
+}
+
+export default function DevsPage() {
+ return (
+  <MainPage/>
+ )
+}
