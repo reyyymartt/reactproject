653741ee44143561d131015ccc02b7d453b3ffc2
reyyymartt <reymartcenteno03@gmail.com>
2024-09-11 09:48:38 +0800

    update

--- app.js
+++ app.js
@@ -2,38 +2,49 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import HomePage from './pages/home'
 import AboutPage from './pages/about'
+import DevsPage from './pages/developers'
 
+const content = document.getElementById('content')
 
 const routes = {
  'home': HomePage(),
  'about': AboutPage(),
- 'developers': null
+ 'devs': DevsPage()
 }
 
 const hiddenRoutes={
  'viewdevs': null
 }
 
-ReactDOM.render(HomePage(),document.getElementById("content")
- )
+ReactDOM.render(HomePage(),content)
  
- 
- function navigate (route){
-  const page = routes[route]?route:NaN
-  
+function navigate (route){
+ for (const x in routes){
+  const btn = document.querySelector(`a[data-route=${x}]`)
+  btn.parentElement.classList.remove('active')
  }
  
+ const navbtn = document.querySelector(`a[data-route=${route}]`)
+ navbtn.parentElement.classList.add('active')
  
+ ReactDOM.render(routes[route],content)
+}
+
 for (const x in routes){
  const btn = document.querySelector(`a[data-route=${x}]`)
- if (btn!=null&&routes[x]!=null){
+ 
+ if (btn!=null){
   btn.onclick=()=>{
-   ReactDOM.render(routes[x],document.getElementById('content'))
    navigate(x)
   }
  } else {
-  console.error(`a[data-route=${x}] not found in navigation`)
+  console.error(`[${x}] is not a page`)
  }
+ 
 }
  
 feather.replace()
+
+
+//exports
+export default navigate
