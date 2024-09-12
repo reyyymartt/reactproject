c48234bd388a5d452f97338ae6be7792acd1a301
reyyymartt <reymartcenteno03@gmail.com>
2024-09-12 11:05:39 +0800

    update about.js

--- pages/about.js
+++ pages/about.js
@@ -7,22 +7,28 @@ import navigate from '/app'
 class APage extends React.Component{
  
  render (){
-  const cardstyle = {
-   margin:'5px'
+  const AboutPageStyle = {
+   padding:'0px'
   }
   return (
-   <div>
-   <div style={cardstyle} className="card">
+   <div style={AboutPageStyle}>
+   <div className="m-2 card">
    <div className="card-body">
    <h3 className="card-title">{this.props.tit}</h3>
    <p className="card-text">{this.props.txt}</p>
    </div>
    </div>
-   <button style={{margin:'5px'}} onClick={
+   <div className="m-2 border rounded card-body">
+   <h3 className="card-title">Luloy's storage</h3>
+   <div className="progress bg-dark">
+   <div style={{width:'23%'}} className="progress-bar progress-bar-striped progress-bar-animated">23%</div>
+   </div>
+   </div>
+   <button onClick={
    function (){
    navigate('home')
    }
-   } className="btn btn-danger" >Go to home</button>
+   } className="m-2 btn btn-danger" >Go to home</button>
    </div>
    )
  }
